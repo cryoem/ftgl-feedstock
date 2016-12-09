fc41ee6b120dabec3f8068962a1e540a22cc2679
shadow_walker <shadowwalkersb@gmail.com>
2016-12-08 23:10:40 -0600

    export envars: FREETYPE and LD_LIBRARY

--- recipe/build.sh
+++ recipe/build.sh
@@ -1,5 +1,8 @@
 #!/bin/bash
 
+export FREETYPE=${PREFIX}
+export LD_LIBRARY_PATH=$PREFIX/lib:$LD_LIBRARY_PATH
+
 if [ "$(uname)" == "Linux" ]; then
 	./configure --prefix=$PREFIX --enable-shared LIBS="-lGL -lGLU -lm -ldl -lXxf86vm -lpthread"
 elif [ "$(uname)" == "Darwin" ]; then
