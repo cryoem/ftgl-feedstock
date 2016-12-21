a8fd176765873f38e6c5d6611b29154ed5c21391
shadow_walker <shadowwalkersb@gmail.com>
2016-12-20 23:37:39 -0600

    initial commit: bld.bat

---
+++ recipe/bld.bat
@@ -0,0 +1,12 @@
+SET builddir=%SRC_DIR%\\..\\build_dir
+
+rmdir /s /q %builddir%
+if errorlevel 1 exit 1
+mkdir  %builddir% && cd %builddir%
+if errorlevel 1 exit 1
+
+cmake %SRC_DIR% -G "Visual Studio 9 2008 Win64" -DCMAKE_BUILD_TYPE=Release -DCMAKE_INSTALL_PREFIX=%LIBRARY_PREFIX%
+if errorlevel 1 exit 1
+
+cmake --build %builddir% --config Release --target install
+if errorlevel 1 exit 1
