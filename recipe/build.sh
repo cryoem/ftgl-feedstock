db30c5e7f0c118a75bcc37ce4f2db61853f0a14e
shadow_walker <shadowwalkersb@gmail.com>
2020-02-13 23:25:13 -0600

    /usr/lib64/

--- recipe/build.sh
+++ recipe/build.sh
@@ -3,7 +3,7 @@
 export CXXFLAGS=${CXXFLAGS/-std=c++14/}
 
 ./configure --prefix=$PREFIX \
-            --with-gl-lib=${BUILD_PREFIX}/${HOST}/sysroot/usr/lib64/libGL.so \
+            --with-gl-lib=${BUILD_PREFIX}/${HOST}/sysroot/usr/lib64/ \
             --disable-freetypetest
 
 make
