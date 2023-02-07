9e8aa406c9ddcad67e2be898d7d8422b311f4251
shadow_walker <shadowwalkersb@gmail.com>
2023-02-06 20:45:09 -0600

    cmake from cf main

--- recipe/build.sh
+++ recipe/build.sh
@@ -1,8 +1,10 @@
-#!/bin/bash
+#!/usr/bin/env bash
+# Enable bash strict mode
+# http://redsymbol.net/articles/unofficial-bash-strict-mode/
+set -ex
 
-set -x
+cmake ${CMAKE_ARGS} -DCMAKE_INSTALL_PREFIX=${PREFIX} -DCMAKE_BUILD_TYPE=Release -S . -B build 
 
-cmake . -DCMAKE_INSTALL_PREFIX=${PREFIX}
+cmake --build build
 
-make -j${CPU_COUNT}
-make install
+cmake --install build
