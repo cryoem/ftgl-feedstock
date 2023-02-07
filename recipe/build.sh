0fca26fab243b8186a7e00a10ae0d860f8b024a5
shadow_walker <shadowwalkersb@gmail.com>
2023-02-07 00:41:18 -0600

    revert glu

--- recipe/build.sh
+++ recipe/build.sh
@@ -5,7 +5,7 @@ set -x
 ./autogen.sh
 #cmake . -DCMAKE_INSTALL_PREFIX=${PREFIX}
 
-./configure --prefix=${PREFIX} --disable-freetypetest --without-glu --disable-dependency-tracking
+./configure --prefix=${PREFIX} --disable-freetypetest --disable-dependency-tracking
 
 make -j${CPU_COUNT}
 make install
