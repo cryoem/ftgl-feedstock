22471747cbdb1bfa49bf4ad7c7042e38412d1c53
shadow_walker <shadowwalkersb@gmail.com>
2023-02-07 00:30:48 -0600

    --without-glu

--- recipe/build.sh
+++ recipe/build.sh
@@ -5,7 +5,7 @@ set -x
 ./autogen.sh
 #cmake . -DCMAKE_INSTALL_PREFIX=${PREFIX}
 
-./configure --prefix=${PREFIX} --disable-freetypetest
+./configure --prefix=${PREFIX} --disable-freetypetest --without-glu
 
 make -j${CPU_COUNT}
 make install
